bb7db13a3bf4c8c97a48c2ea82e754d3ff7a446c
manhd89 <150607244+manhd89@users.noreply.github.com>
2026-03-14 08:33:36 +0700

    Thêm tìm kiếm

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./HomeScreen";
 import DetailScreen from "./DetailScreen";
+import SearchScreen from "./SearchScreen";
+import { TouchableOpacity, Text } from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -9,8 +11,25 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "Home",
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ marginRight: 15 }}
+                onPress={() => navigation.navigate("Search")}
+              >
+                <Text style={{ color: "#2196F3", fontWeight: "bold" }}>
+                  🔍 Tìm kiếm
+                </Text>
+              </TouchableOpacity>
+            ),
+          })}
+        />
         <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
